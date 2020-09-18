1a3f594a8ca8dabed958fad0c1de1fc9a03c1d83
Rina <nenegrakrina@gmail.com>
2020-09-18 16:17:55 +0400

    Это наш первый комит

---
+++ js/hw.js
@@ -0,0 +1,14 @@
+var age,
+  name,
+  mailing;
+
+function access(){
+  alert("Доступ к сайту разрешён.");
+  console.log("Пользователь допущен.");
+}
+
+if(age >= 18){
+  if(mailing = true){
+    access();
+  }
+}
---
+++ js/script.js
@@ -0,0 +1,79 @@
+// "Comment me!"
+
+/* "And"
+    "ME!"*/
+
+// alert("Hello world!");
+
+// console.log("Hello world!");
+
+/*var myFirstVar = 50;
+
+myFirstVar = 25;
+
+console.log(myFirstVar);*/
+
+/*console.log(4+6);
+console.log(4+ "I am string");
+console.log("Hello" + " World");*/
+
+/*var incr = 10,
+    descr = 10;
+
+incr++;
+descr++;
+
+console.log(incr);
+console.log(descr);*/
+
+// console.log(10%3);
+
+/*var num = "5";
+console.log(num != 5);
+console.log(num == 5);
+console.log(num === 5);*/
+
+/*var number = 5;
+var string = "Hello World";
+var symbol;
+true;
+false;*/
+
+/*console.log(6/2 == 3);
+console.log(10/5 == 3);*/
+
+// var und;
+
+// console.log(und);
+
+// var obj = {};
+
+/*console.log(typeof obj);
+console.log(typeof string)*/
+
+/*if ( 10 + 5 == 15) {
+  alert("It's a true!!");
+}
+else if ( 10 + 5 == 14) {
+  alert("Wrong!");
+}*/
+
+/*for (var i = 1; i < 8; i++) {
+  console.log(i);
+};*/
+
+/*
+var out = 5;
+
+function myFirstFunc(x,y) {
+  var inner = 10;
+
+  //console.log(out);
+
+  return (x + y);
+}
+console.log(myFirstFunc(5,5));
+console.log(myFirstFunc(10,5));
+console.log(myFirstFunc(5,101));
+//console.log(inner);
+*/
